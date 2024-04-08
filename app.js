2dbb6b25c7e359f34eb777f1d49dbd765c7b19ff
Pongpipat siangsai <163412313+Fah-Pongpipat@users.noreply.github.com>
2024-04-08 13:46:26 +0700

    add debug

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ import("chalk")
 
     const app = express();
     const port = 3000;
+    const debug = require("debug")("app");
 
     app.get("/", (req, res) => {
       res.send("Hello Fah");
     });
 
     app.listen(port, () => {
-      console.log(`Server is running on port ${chalk.green(": ", port)}`);
+      debug(`Server is running on port ${chalk.green(": ", port)}`);
     });
   })
   .catch((err) => {
