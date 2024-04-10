af36480f2eff15875e78244aa52f459083555dc3
Pongpipat siangsai <163412313+Fah-Pongpipat@users.noreply.github.com>
2024-04-10 11:26:03 +0700

    install nodemon

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 const debug = require("debug")("app");
-const morgan = require("morgan");
-
-app.use(morgan("combined"));
-
+app.use(express.static(path.join(__dirname, "/public/")));
 app.get("/", (req, res) => {
   res.send("Hello Fah");
 });
