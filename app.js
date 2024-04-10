ab8d6c8fdbcfae46c43e9e79ced60f8fe38376dd
Pongpipat siangsai <163412313+Fah-Pongpipat@users.noreply.github.com>
2024-04-10 11:26:49 +0700

    Revert "install nodemon"
    This reverts commit af36480f2eff15875e78244aa52f459083555dc3.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const path = require("path");
 const debug = require("debug")("app");
-app.use(express.static(path.join(__dirname, "/public/")));
+const morgan = require("morgan");
+
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
   res.send("Hello Fah");
 });
